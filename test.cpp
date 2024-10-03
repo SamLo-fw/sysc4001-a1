0d6642dc6c8f77997995719801de3dea69f58f9f
Samuel Lo <sam.lo71903@gmail.com>
2024-10-02 22:42:43 -0400

    init commit for remote editing

---
+++ test.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+    //exit the program if no trace file provided
+    if(argc < 2){
+        perror("error, missing trace filename");
+        return 1;
+    }
+
+    //getting the file url based on the input argument
+    char *furl_start = ".\\";
+    char *furl_full = NULL;
+    int furl_size = 2+strlen(argv[1]);
+    furl_full = (char*) malloc(furl_size);
+
+    //lol this is so cursed already
+    strcpy(furl_full, furl_start);
+    strcpy(furl_full + 2, argv[1]);
+
+    //get the trace filepath 
+    FILE *fptr;
+    printf("trying to locate trace file %s \n", furl_full);
+    if((fptr = fopen(furl_full, "r")) == NULL){
+        perror("could not find trace file.");
+        return 1;
+    }
+    printf("found file %s \n", furl_full);
+
+    //write output
+    FILE *optr;
+    printf("trying to write output file");
+    if(optr = fopen(".\\execution1.txt", "w")){
+        perror("could not open output file.");
+        return 1;
+    }
+
+    fclose(fptr);
+    //free mallocs before return
+    free(furl_full);
+
+    return 0;
+}
